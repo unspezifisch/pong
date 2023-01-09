db7ab500e2eb04c91eb7db51dc87116cc2be0df2
unspezifisch <122180252+unspezifisch@users.noreply.github.com>
2023-01-09 17:17:19 +0100

    Add files via upload

--- pong.service.worker.js
+++ pong.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1673187603|31282048";
+const CACHE_VERSION = "1673280971|6113465515";
 const CACHE_PREFIX = "pong-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "pong.offline.html";
